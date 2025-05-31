2090a539d2290bff2f83e8e412d86ca9b6f92423
edvaldinhs <edvaldohenriqueceu@gmail.com>
2025-05-31 10:14:20 -0300

    ♦:cors addition to prototype creation

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 import express from 'express';
+import cors from 'cors'
 import produtoRoutes from './routes/produto.js';
 import userRoutes from './routes/user.js';
 
 const app = express();
 app.use(express.json());
+
+//Cuidado com esse cors, lembrar de configurar!!
+app.use(cors())
 
 app.use('/produto', produtoRoutes);
 app.use('/user', userRoutes);
